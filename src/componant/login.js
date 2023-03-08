ca3bb81b8fa55cc9b5f4e1e71548447d4f7d4a20
Sammed <sammedgudale31@gmail.com>
2023-03-08 18:04:16 +0530

    login

--- src/componant/login.js
+++ src/componant/login.js
@@ -1,49 +1,90 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const login = () => {
+
+import { signin } from "../action/UserActions";
+
+import { useDispatch, useSelector } from "react-redux";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userSignin = useSelector((store) => store.userSignin);
+  const { loading, error, response } = userSignin;
+
+  const dispatch = useDispatch();
+
+  const OnSignin = () => {
+    dispatch(signin(email, password));
+  };
   return (
-    <div>
-      <section class="vh-100">
-  <div class="container py-5 h-100">
-    <div class="row d-flex align-items-center justify-content-center h-100">
-      <div class="col-md-8 col-lg-7 col-xl-6">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-          class="img-fluid" alt="Phone image"/>
-      </div>
-      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+   <>
+    <div className=" container col-sm-10 text-center mt-5">
+      <div className="container col-sm-6 bg-light mt-5 border border-dark border-5">
         <form>
-         
-          <div class="form-outline mb-4">
-            <input type="email" id="form1Example13" class="form-control form-control-lg" />
-            <label class="form-label" for="form1Example13">Email address</label>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
           </div>
 
-          
-          <div class="form-outline mb-4">
-            <input type="password" id="form1Example23" class="form-control form-control-lg" />
-            <label class="form-label" for="form1Example23">Password</label>
-          </div>
+          <Link
+            to="/"
+            onClick={OnSignin}
+            type="submit"
+            className="btn btn-primary float-left"
+          >
+            Login
+          </Link>
 
-          <div class="d-flex justify-content-around align-items-center mb-4">
-           
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-              <label class="form-check-label" for="form1Example3"> Remember me </label>
-            </div>
-            <a href="#!">Forgot password?</a>
-          </div>
+          <Link
+            type="button"
+            className="btn btn-outline-primary float-left ml-4"
+            to="/forgotpass"
+          >
+            Forgot Password
+          </Link>
 
-         
-          <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+          <p className="col-sm-auto float-left ml-2 ">New User?</p>
 
-
+          <Link
+            type="button"
+            className="btn btn-outline-primary float-right"
+            to="/registration"
+          >
+            Register Here
+          </Link>
+          <br></br>
+          <br></br>
         </form>
       </div>
     </div>
-  </div>
-</section>
-    </div>
+   </>
   )
 }
 
-export default login
+export default Login
